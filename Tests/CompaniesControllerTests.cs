86730c01aeadc639e447456c9998fc76ca95ed1a
Michel Nickbon <michel.lemponen@gmail.com>
2020-09-18 21:08:15 +0200

    Added foundation for controller logic tests

--- Tests/CompaniesControllerTests.cs
+++ Tests/CompaniesControllerTests.cs
@@ -1,7 +1,16 @@
 ﻿
+using ApiService.Controllers;
+using System.Threading.Tasks;
+using Xunit;
+
 namespace ApiService.Tests
 {
 	public class CompaniesControllerTests
 	{
+		[Fact]
+		public async Task GetCompanyList_ReturnsList()
+		{
+			// Coming soon
+		}
 	}
 }
